3fb59cc1caa3e48a32d50a6495ce0a81a079534e
Wen Wang (王文) <wwang.bio@gmail.com>
2025-05-23 17:22:00 -0400

    Update: version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ copyright = '2024-2025, Guo-Cheng Yuan Lab'
 author = 'Wen Wang, Shiwei Zheng, Crystal Shin, Joselyn Chavez, Guo-Cheng Yuan'
 
 release = '1.0'
-version = '1.1.3'
+version = '1.1.4'
 
 # -- General configuration
 
